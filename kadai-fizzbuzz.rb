46fed7cc77c87c8e694eba9085f1b6cad47039f5
six.kone <rainbowsixsiege1999s@gmail.com>
2021-06-12 12:55:38 +0000

    kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,19 @@
+num_max = 100
+
+def fizzbuzz(num)
+  
+  if (num % 3 == 0) && (num % 5 == 0) 
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else
+    num.to_s
+  end
+  
+end 
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
